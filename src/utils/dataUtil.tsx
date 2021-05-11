aca151891cff5c2cd811e9c82ebe703b9c115bda
Sameer Rai <sameerai2736@gmail.com>
2021-05-11 12:30:41 +0545

    Add StackedBarChart  and  use it to show main activity information

--- src/utils/dataUtil.tsx
+++ src/utils/dataUtil.tsx
@@ -3,6 +3,8 @@ import { createTextColumn } from '../Components/JipsTable/tools/tableHelpers';
 import CellBar from '../Components/JipsTableBar/CellBar';
 import { SubSection, Values } from "../types";
 import { bgColors, colors } from './colorUtil';
+import { listToGroupList, mapToList } from '@togglecorp/fujs';
+
 
 export interface tableData {
     columns:any[],
@@ -117,6 +119,32 @@ export const getBarChartData = (props:GetBarChartProps) => {
         })
         return k;
     });
-    
     return data;
 }
+
+
+export const getActivityData = (data: SubSection) => {
+    const combined = data.vars.flatMap((v: Values) => {
+        const { data, variable } = v;
+        const [activity, gender] = variable.split(' - ');
+        const { keys, values } = data;
+        const activities = keys.map((k: string, index: number) => ({
+            key: k,
+            [activity]: Math.round(values[index]*100),
+            gender: gender.trim().toLowerCase(),
+        }));
+        return activities;
+    });
+
+    return combined;
+}
+
+interface Activity {
+    key: string;
+    gender: string;
+}
+
+export const getGenderActivityData = (data: Activity[], key: string) => mapToList(
+    listToGroupList(data.filter(v => v.key === key), v => v.gender),
+    v => Object.assign({}, ...v),
+)
